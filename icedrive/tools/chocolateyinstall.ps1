5fa4e449b356967addd0e994ede9beee1cce2bf3
Johannes Zeller <jo.zeller@protonmail.com>
2024-08-01 22:47:32 +0200

    temporarily downgrade to test automatic upgrade

--- icedrive/tools/chocolateyinstall.ps1
+++ icedrive/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64      = 'https://cdn.icedrive.net/static/apps/win/Icedrive_Installer-2.75.exe'
+$url64      = 'https://cdn.icedrive.net/static/apps/win/Icedrive_Installer-2.74.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url64bit      = $url64
   softwareName  = 'icedrive*'
-  checksum64    = '87AF441AC5505E30A476AAD3FCF3157DACC1288AA96BEE26A69D603B109F8B79'
+  checksum64    = 'C9FBC0D2BD42F1DE8E7D612B2AF65AA643AD75E923CA0AE5D1BD45E9C29A7934'
   checksumType64= 'sha256'
   silentArgs   = '/S'
   validExitCodes= @(0, 3010, 1641)
