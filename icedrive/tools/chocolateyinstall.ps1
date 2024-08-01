308e89cd570e6be14320ee4ff2b6c324b261a1a9
jzelBot <jzelbot.creatable458@passinbox.com>
2024-08-02 00:51:06 +0200

    Automated update commit

--- icedrive/tools/chocolateyinstall.ps1
+++ icedrive/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64      = 'https://cdn.icedrive.net/static/apps/win/Icedrive_Installer-2.74.exe'
+$url64      = 'https://cdn.icedrive.net/static/apps/win/Icedrive_Installer-2.75.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url64bit      = $url64
   softwareName  = 'icedrive*'
-  checksum64    = 'C9FBC0D2BD42F1DE8E7D612B2AF65AA643AD75E923CA0AE5D1BD45E9C29A7934'
+  checksum64    = '87af441ac5505e30a476aad3fcf3157dacc1288aa96bee26a69d603b109f8b79'
   checksumType64= 'sha256'
   silentArgs   = '/S'
   validExitCodes= @(0, 3010, 1641)
